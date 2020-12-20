6d4471b880b679b200f17aaaebcc68a4f6d78603
johnnyb1509 <150994johnnyb@gmail.com>
2020-12-20 17:28:46 +0700

    clean data v2

--- src/dataprocess/content_filter_on_ticker.py
+++ src/dataprocess/content_filter_on_ticker.py
@@ -8,8 +8,7 @@ Created on Sat Dec 19 12:20:10 2020
 import numpy as np
 import pandas as pd
 import os
-
-os.chdir('D:/shan_github/2020_VBDI_BML')
+os.chdir('D:/VBDI_BML')
 
 data_processed = pd.read_csv("./data/data_preprocess.csv")
 
@@ -18,7 +17,7 @@ tickers = ['bid', 'ctg', 'vcb', 'stb', 'ssi', 'vic', 'fpt', 'mwg', 'pnj', 'msn']
 
 list_news = {}
 for ticker in tickers:
-    print('getting ticker {}'.format(ticker))
+    print('getting ticker {}'.format(ticker.upper()))
     list_news[ticker] = data_processed[data_processed['Ticker'] == ticker.upper()].dropna(subset=['PublicDate'])
     print('datefrom: {}'.format(list_news[ticker]['PublicDate'].values[0]))
     print('dateto: {}'.format(list_news[ticker]['PublicDate'].values[-1]))
